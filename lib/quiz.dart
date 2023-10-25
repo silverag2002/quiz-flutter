83c3f0f7cdbbf556b1f4069a24f1c39878dd60be
silverag2002 <silverag2002@gmail.com>
2023-10-25 09:01:08 +0530

    learned how to pass function as contructor

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/splash.dart';
+import 'package:quiz_app/questions_screen.dart';
 
 class QuizWidget extends StatefulWidget {
   const QuizWidget({super.key});
@@ -9,33 +11,53 @@ class QuizWidget extends StatefulWidget {
 }
 
 class _QuizWidgetState extends State<QuizWidget> {
+  void onPressed() {
+    setState(() {
+      activeScreen = const QuestionsWidget();
+    });
+  }
+
+  Widget? activeScreen;
+  @override
+  void initState() {
+    activeScreen = SplashScreen(onPressed);
+    super.initState();
+  }
+
   @override
   Widget build(context) {
+    // return MaterialApp(
+    //     home: Scaffold(
+    //         body: (Container(
+    //             decoration: const BoxDecoration(
+    //                 color: Color.fromARGB(255, 71, 39, 160)),
+    //             child: Center(
+    //                 child: Column(mainAxisSize: MainAxisSize.min, children: [
+    //               Image.asset('assets/images/quiz-logo.png',
+    //                   width: 200, color: Colors.purpleAccent),
+    //               const Padding(
+    //                 padding: EdgeInsets.all(40.0),
+    //                 child: Text(
+    //                   "Learn Flutter this way",
+    //                   style: TextStyle(
+    //                     fontSize: 18,
+    //                     color: Colors.purpleAccent,
+    //                   ),
+    //                 ),
+    //               ),
+    //               OutlinedButton.icon(
+    //                   onPressed: () {},
+    //                   style: OutlinedButton.styleFrom(
+    //                       foregroundColor: Colors.purpleAccent),
+    //                   icon: Icon(Icons.arrow_right_alt),
+    //                   label: const Text("Try Now"))
+    //             ]))))));
+
     return MaterialApp(
         home: Scaffold(
             body: (Container(
                 decoration: const BoxDecoration(
                     color: Color.fromARGB(255, 71, 39, 160)),
-                child: Center(
-                    child: Column(mainAxisSize: MainAxisSize.min, children: [
-                  Image.asset('assets/images/quiz-logo.png',
-                      width: 200, color: Colors.purpleAccent),
-                  const Padding(
-                    padding: EdgeInsets.all(40.0),
-                    child: Text(
-                      "Learn Flutter this way",
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.purpleAccent,
-                      ),
-                    ),
-                  ),
-                  OutlinedButton.icon(
-                      onPressed: () {},
-                      style: OutlinedButton.styleFrom(
-                          foregroundColor: Colors.purpleAccent),
-                      icon: Icon(Icons.arrow_right_alt),
-                      label: const Text("Try Now"))
-                ]))))));
+                child: activeScreen))));
   }
 }
--- lib/splash.dart
+++ lib/splash.dart
@@ -1,36 +1,31 @@
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
-  void onPressCheck() {
-    print("Ankit");
-  }
+  const SplashScreen(this.onPressed, {super.key});
+
+  final void Function() onPressed;
 
   @override
   Widget build(context) {
-    return (Container(
-        decoration:
-            const BoxDecoration(color: Color.fromARGB(255, 71, 39, 160)),
-        child: Center(
-            child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Image.asset('assets/images/quiz-logo.png',
-              width: 200, color: Colors.purpleAccent),
-          const Padding(
-            padding: EdgeInsets.all(40.0),
-            child: Text(
-              "Learn Flutter this way",
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.purpleAccent,
-              ),
-            ),
+    return Center(
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+      Image.asset('assets/images/quiz-logo.png',
+          width: 200, color: Colors.purpleAccent),
+      const Padding(
+        padding: EdgeInsets.all(40.0),
+        child: Text(
+          "Learn Flutter this way",
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.purpleAccent,
           ),
-          OutlinedButton.icon(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(
-                  foregroundColor: Colors.purpleAccent),
-              icon: Icon(Icons.arrow_right_alt),
-              label: const Text("Try Now"))
-        ]))));
+        ),
+      ),
+      OutlinedButton.icon(
+          onPressed: onPressed,
+          style: OutlinedButton.styleFrom(foregroundColor: Colors.purpleAccent),
+          icon: Icon(Icons.arrow_right_alt),
+          label: const Text("Try Now"))
+    ]));
   }
 }
